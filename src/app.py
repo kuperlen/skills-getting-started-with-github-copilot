29363976af39fe5e81ce109f12490413e331bf0b
Leon Kuperman <leonkuperman@Leons-MacBook-Pro.local>
2026-06-02 16:52:17 -0400

    Complete application updates and improvements

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ A super simple FastAPI application that allows students to view and sign up
 for extracurricular activities at Mergington High School.
 """
 
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
@@ -65,3 +66,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
